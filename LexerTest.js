b7b3f0b35df66ef68969379ceb34b1d455b2fbea
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-07-15 23:11:57 -0700

    Add the simpler interactive test only for the lexer.

---
+++ LexerTest.js
@@ -0,0 +1,77 @@
+/*
+  Copyright (C) 2011 Ariya Hidayat <ariya.hidayat@gmail.com>
+  Copyright (C) 2010 Ariya Hidayat <ariya.hidayat@gmail.com>
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*global TapDigit:true */
+var lexer, parseId;
+
+function parse() {
+    if (parseId) {
+        window.clearTimeout(parseId);
+    }
+
+    parseId = window.setTimeout(function () {
+        var code, str,
+            lexer, tokens, token, i;
+
+        code = document.getElementById('code').value;
+        try {
+            if (typeof lexer === 'undefined') {
+                lexer = new TapDigit.Lexer();
+            }
+
+            tokens = [];
+            lexer.reset(code);
+            while (true) {
+                token = lexer.next();
+                if (typeof token === 'undefined') {
+                    break;
+                }
+                tokens.push(token);
+            }
+
+            str = '<table width=200>\n';
+            for (i = 0; i < tokens.length; i += 1) {
+                token = tokens[i];
+                str += '<tr>';
+                str += '<td>';
+                str += token.type;
+                str += '</td>';
+                str += '<td align=center>';
+                str += token.value;
+                str += '</td>';
+                str += '</tr>';
+                str += '\n';
+            }
+            document.getElementById('tokens').innerHTML = str;
+
+        } catch (e) {
+            document.getElementById('tokens').innerText = '';
+        }
+        parseId = undefined;
+    }, 345);
+}
