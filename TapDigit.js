780f2a5ed7d00e193bed8ad0251d382f807046df
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-07-19 21:38:18 -0700

    Editor: rewritten from scratch.
    This is still based on proxy input strategy.
    Text selection (using arrow keys) now works.
    
    Editing code has been merged into TapDigit namespace.

--- TapDigit.js
+++ TapDigit.js
@@ -26,6 +26,7 @@
   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/*global document:true,window:true */
 var TapDigit;
 TapDigit = TapDigit || {};
 
@@ -463,6 +464,250 @@ TapDigit.Parser = function () {
 
     return {
         parse: parse
+    };
+};
+
+TapDigit.Editor = function (element) {
+
+    var input, editor, cursor, blinkTimer, lexer, hasFocus;
+
+    function hideCursor() {
+        if (blinkTimer) {
+            window.clearInterval(blinkTimer);
+        }
+        blinkTimer = undefined;
+        cursor.style.visibility = 'hidden';
+    }
+
+    function blinkCursor() {
+        var visible = true;
+        if (blinkTimer) {
+            window.clearInterval(blinkTimer);
+        }
+        blinkTimer = window.setInterval(function () {
+            cursor.style.visibility = visible ? '' : 'hidden';
+            visible = !visible;
+        }, 423);
+    }
+
+    // Get cursor position from the proxy input and adjust the editor
+    function updateCursor() {
+        var start, end, x, y, el, cls;
+
+        if (typeof cursor === 'undefined') {
+            return;
+        }
+
+        if (cursor.getAttribute('id') !== 'cursor') {
+            return;
+        }
+
+        start = input.selectionStart;
+        end = input.selectionEnd;
+
+        if (editor.childElementCount <= start) {
+            return;
+        }
+
+        el = editor.childNodes[start];
+        if (el) {
+            x = el.offsetLeft;
+            y = el.offsetTop;
+            cursor.style.left = x + 'px';
+            cursor.style.top = y + 'px';
+            cursor.style.opacity = 1;
+        }
+
+        // If there is a selection, add the CSS class 'selected'
+        // to all nodes inside the selection range.
+        while (start < end) {
+            cursor.style.opacity = 0;
+            el = editor.childNodes[start];
+            if (el) {
+                cls = el.getAttribute('class');
+                cls += ' selected';
+                el.setAttribute('class', cls);
+            }
+            start += 1;
+        }
+    }
+
+    // Get a new text from the proxy input and update the syntax highlight
+    function updateEditor() {
+        var expr, tokens, token, i, j, text, str, html;
+
+        if (typeof lexer === 'undefined') {
+            lexer = new TapDigit.Lexer();
+        }
+
+        tokens = [];
+        try {
+            expr = input.value;
+            lexer.reset(expr);
+            while (true) {
+                token = lexer.next();
+                if (typeof token === 'undefined') {
+                    break;
+                }
+                tokens.push(token);
+            }
+
+            text = '';
+            html = '';
+            for (i = 0; i < tokens.length; i += 1) {
+                token = tokens[i];
+                j = 0;
+                while (text.length < token.start) {
+                    text += ' ';
+                    html += '<span class="blank"> </span>';
+                    j = 1;
+                }
+                str = expr.substring(token.start, token.end + 1);
+                for (j = 0; j < str.length; j += 1) {
+                    html += '<span class="' + token.type + '">';
+                    html += str.charAt(j);
+                    text += str.charAt(j);
+                    html += '</span>';
+                }
+            }
+            while (text.length < expr.length) {
+                text += ' ';
+                html += '<span class="blank"> </span>';
+            }
+        } catch (e) {
+            // plain spans for the editor
+            html = '';
+            for (i = 0; i < expr.length; i += 1) {
+                html += '<span class="error">' + expr.charAt(i) + '</span>';
+            }
+        } finally {
+            html += '<span class="cursor" id="cursor">&nbsp;</span>';
+            if (html !== editor.innerHTML) {
+                editor.innerHTML = html;
+                cursor = document.getElementById('cursor');
+                blinkCursor();
+                updateCursor();
+            }
+        }
+    }
+
+    function focus() {
+        window.setTimeout(function () {
+            input.focus();
+            blinkCursor();
+            updateCursor();
+        }, 0);
+    }
+
+    function blur() {
+        input.blur();
+    }
+
+    function deselect() {
+        var el, cls;
+        input.selectionEnd = input.selectionStart;
+        el = editor.firstChild;
+        while (el) {
+            cls = el.getAttribute('class');
+            if (cls.match('selected')) {
+                cls = cls.replace('selected', '');
+                el.setAttribute('class', cls);
+            }
+            el = el.nextSibling;
+        }
+    }
+
+    function onInputKeyDown(event) {
+        updateCursor();
+    }
+
+    function onInputKeyUp(event) {
+        updateEditor();
+    }
+
+    function onInputBlur() {
+        hasFocus = false;
+        hideCursor();
+    }
+
+    function onInputFocus() {
+        hasFocus = true;
+    }
+
+    function onEditorMouseDown(event) {
+        var x, y, i, el, x1, y1, x2, y2;
+
+        deselect();
+
+        // Assume first that it is at the end
+        input.selectionStart = input.value.length;
+        input.selectionEnd = input.selectionStart;
+
+        x = event.clientX;
+        y = event.clientY;
+        for (i = 0; i < editor.childElementCount; i += 1) {
+            el = editor.childNodes[i];
+            x1 = el.offsetLeft;
+            x2 = x1 + el.offsetWidth;
+            y1 = el.offsetTop;
+            y2 = y1 + el.offsetHeight;
+            if (x1 <= x && x < x2 && y1 <= y && y < y2) {
+                input.selectionStart = i;
+                input.selectionEnd = i;
+                blinkCursor();
+                break;
+            }
+        }
+
+        focus();
+    }
+
+    function setupDOM(element) {
+        var container, wrapper;
+
+        // Proxy input where we capture user keyboard interaction
+        input = document.createElement('input');
+        input.style.position = 'absolute';
+        input.style.width = '100px';
+        input.value = 'x = 40 + (6 / 3.0)';
+        input.style.position = 'absolute';
+
+        // Container for the above proxy, it also hides the proxy element
+        container = document.createElement('div');
+        container.appendChild(input);
+        container.style.overflow = 'hidden';
+        container.style.width = '1px';
+        container.style.height = '0px';
+        container.style.position = 'relative';
+
+        // The "fake" editor
+        editor = document.createElement('div');
+        editor.setAttribute('class', 'editor');
+        editor.style.wrap = 'on';
+        editor.textContent = ' ';
+
+        // Top-level wrapper for container
+        wrapper = document.createElement('div');
+        wrapper.appendChild(container);
+        wrapper.appendChild(editor);
+        element.appendChild(wrapper);
+
+        // Wire all event handlers
+        input.addEventListener('keydown', onInputKeyDown, false);
+        input.addEventListener('keyup', onInputKeyUp, false);
+        input.addEventListener('blur', onInputBlur, false);
+        input.addEventListener('focus', onInputFocus, false);
+        editor.addEventListener('mousedown', onEditorMouseDown, false);
+    }
+
+    hasFocus = false;
+    setupDOM(element);
+    updateEditor();
+
+    return {
+        focus: focus,
+        blur: blur,
+        deselect: deselect
     };
 };
 
