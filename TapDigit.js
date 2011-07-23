a0e1b33831e831be039c6f69fae6cde73f52f05b
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-07-22 23:22:36 -0700

    Evaluator: first attempt at the evaluator.

--- TapDigit.js
+++ TapDigit.js
@@ -467,6 +467,72 @@ TapDigit.Parser = function () {
     };
 };
 
+TapDigit.Evaluator = function () {
+
+    var parser = new TapDigit.Parser();
+
+    function exec(node) {
+        var left, right, expr;
+
+        if (node.hasOwnProperty('Expression')) {
+            return exec(node.Expression);
+        }
+
+        if (node.hasOwnProperty('Number')) {
+            return parseFloat(node.Number);
+        }
+
+        if (node.hasOwnProperty('Binary')) {
+            node = node.Binary;
+            left = exec(node.left);
+            right = exec(node.right);
+            switch (node.operator) {
+            case '+':
+                return left + right;
+            case '-':
+                return left - right;
+            case '*':
+                return left * right;
+            case '/':
+                return left / right;
+            default:
+                throw new SyntaxError('Unknown operator ' + node.operator);
+            }
+        }
+
+        if (node.hasOwnProperty('Unary')) {
+            node = node.Unary;
+            expr = exec(node.expression);
+            switch (node.operator) {
+            case '+':
+                return expr;
+            case '-':
+                return -expr;
+            default:
+                throw new SyntaxError('Unknown operator ' + node.operator);
+            }
+        }
+
+        // TODO: assign to variable in the context
+        if (node.hasOwnProperty('Assignment')) {
+            return exec(node.Assignment.value);
+        }
+
+        // TODO: handle function call
+
+        throw new SyntaxError('Unknown syntax node');
+    }
+
+    function evaluate(expr) {
+        var tree = parser.parse(expr);
+        return exec(tree);
+    }
+
+    return {
+        evaluate: evaluate
+    };
+};
+
 TapDigit.Editor = function (element) {
 
     var input, editor, cursor, blinkTimer, lexer, hasFocus;
